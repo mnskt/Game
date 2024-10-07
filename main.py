669114414eefca83286982f7efbc37cdb5879abb
mnskt <mantas.milieska@gmail.com>
2024-10-07 23:35:32 +0300

    - Moved some code around; - Fixed bug for swing animation restarting each time spacebar is pressed; - Added some typehinting; - Added the pathlib module to handle some paths

--- main.py
+++ main.py
@@ -17,37 +17,21 @@ knight = Fighter(x=200, y=320, offset=0, name='Knight', max_hp=30)
 is_running = True
 i = 0
 while is_running:
-    clock.tick(fps)
+    screen.clock.tick(FPS)
     screen.screen.fill((0, 0, 0))
     screen.draw_bg(x=0, y=0, offset_mid=screen.bg_img_mid_offset,
                    offset_back=screen.bg_img_back_offset,
                    offset_front=screen.bg_img_front_offset)
-    if screen.bg_img_front_offset <= -SCREEN_WIDTH:
-        screen.bg_img_front_offset = SCREEN_WIDTH
-    elif screen.bg_img_front_offset >= SCREEN_WIDTH:
-        screen.bg_img_front_offset = -SCREEN_WIDTH
-    if screen.bg_img_mid_offset <= -SCREEN_WIDTH:
-        screen.bg_img_mid_offset = 0
-    elif screen.bg_img_mid_offset >= SCREEN_WIDTH:
-        screen.bg_img_mid_offset = 0
-    if screen.bg_img_back_offset <= -SCREEN_WIDTH:
-        screen.bg_img_back_offset = 0
-    elif screen.bg_img_back_offset >= SCREEN_WIDTH:
-        screen.bg_img_back_offset = 0
-
+    screen.handle_parallax()
     screen.draw_panel(x=0, y=SCREEN_HEIGHT - BOT_PANEL_HEIGHT)
     screen.draw_health_bar(x=BOT_PANEL_HEIGHT / 2 - 23, y=SCREEN_HEIGHT - BOT_PANEL_HEIGHT + 19, fighter=knight)
+
     # for idx, enemy in enumerate(enemy_list):
     #     enemy.draw_fighter()
 
     knight.update()
     knight.draw_fighter()
 
-    if screen.att_anim_counter >= 40:
-        screen.att_anim_counter = 0
-        knight.action = 0
-        knight.frame_index = 0
-        f_hit = False
     # for enemy in enemy_list:
     #     enemy.update()
     #     enemy.draw_fighter()
--- screen.py
+++ screen.py
@@ -1,18 +1,15 @@
 from fighter import *
 from enum import Enum
+from pathlib import WindowsPath
 import pygame
 
 
-pygame.init()
-clock = pygame.time.Clock()
-fps = 60
+FPS = 60
 
 BOT_PANEL_HEIGHT = 150
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 400 + BOT_PANEL_HEIGHT
 
-pygame.display.set_caption('Synergy')
-FONT = pygame.font.SysFont(name='Times New Roman', size=26)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 
@@ -21,10 +18,22 @@ SPEED = 6
 
 class Setup:
     def __init__(self):
+        pygame.init()
+        pygame.display.set_caption('Synergy')
+        self.font = pygame.font.SysFont(name='Times New Roman', size=26)
+        self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-        self.bg_paths = [f'img/Background/parallax_forest/{name}.png' for name in range(1, 4)]
+
+        self.bg_img_path = WindowsPath('img/Background/parallax_forest')
+        self.panel_img_path = WindowsPath(f'{self.bg_img_path.parent.parent}/Icons/panel.png')
+        self.bar_outline_img_path = WindowsPath(f'{self.bg_img_path.parent.parent}/UI/bar.png')
+        self.bar_bg_img_path = WindowsPath(f'{self.bar_outline_img_path.parent}/bar_background.png')
+        self.bar_fg_img_path = WindowsPath(f'{self.bar_bg_img_path.parent}/health_bar.png')
+
+        self.bg_paths = [f'{self.bg_img_path}/{name}.png' for name in range(1, 4)]
         self.bg_layers: list = []
         for idx, layer in enumerate(self.bg_paths):
+
             self.bg_layers.append(pygame.image.load(layer).convert_alpha())
             self.bg_layers[idx] = pygame.transform.scale(
                 self.bg_layers[idx],
@@ -37,16 +46,16 @@ class Setup:
         self.bg_img_back_offset = 0
         self.bg_img_front_offset = 0
 
-        self.panel_img = pygame.image.load('img/Icons/panel.png').convert_alpha()
+        self.panel_img = pygame.image.load(self.panel_img_path).convert_alpha()
         # Health bar
-        self.bar_outline_img = pygame.image.load('img/UI/bar.png').convert_alpha()
+        self.bar_outline_img = pygame.image.load(self.bar_outline_img_path).convert_alpha()
         self.bar_outline_img = pygame.transform.scale(self.bar_outline_img,
                                                       (self.bar_outline_img.get_width() * 3,
                                                        self.bar_outline_img.get_height() * 3))
-        self.bar_bg_img = pygame.image.load('img/UI/bar_background.png').convert_alpha()
+        self.bar_bg_img = pygame.image.load(self.bar_bg_img_path).convert_alpha()
         self.bar_bg_img = pygame.transform.scale(self.bar_bg_img,
                                                  (self.bar_bg_img.get_width() * 3, self.bar_bg_img.get_height() * 3))
-        self.bar_fg_img = pygame.image.load('img/UI/health_bar.png').convert_alpha()
+        self.bar_fg_img = pygame.image.load(self.bar_fg_img_path).convert_alpha()
 
 
 class Layers(Enum):
@@ -79,12 +88,12 @@ class Screen(Setup):
         img = font.render(text, True, text_color)
         self.screen.blit(img, (x, y))
 
-    def draw_panel(self, x, y):
+    def draw_panel(self, x: int | float, y: int | float):
         self.screen.blit(self.panel_img, (x, y))
 
-    def draw_health_bar(self, x, y, fighter):
+    def draw_health_bar(self, x: int | float, y: int | float, fighter):
         self.draw_text(text=f'{fighter.name} HP: {int(fighter.hp)}',
-                       font=FONT, text_color=RED, x=SCREEN_WIDTH / 6, y=SCREEN_HEIGHT - BOT_PANEL_HEIGHT + 50)
+                       font=self.font, text_color=RED, x=SCREEN_WIDTH / 6, y=SCREEN_HEIGHT - BOT_PANEL_HEIGHT + 50)
         # Show player stats
         self.screen.blit(self.bar_bg_img, (x, y))
         fighter = pygame.transform.scale(self.bar_fg_img,
@@ -107,9 +116,31 @@ class Screen(Setup):
             self.bg_img_front_offset += 0.6 * SPEED
 
         if keys[pygame.K_SPACE]:
-            knight.action = 1
+            if self.att_anim_counter == 0:
+                knight.action = 1
+                knight.frame_index = 0
+                f_hit = True
+        if self.att_anim_counter >= 40:
+            self.att_anim_counter = 0
+            knight.action = 0
             knight.frame_index = 0
-            f_hit = True
+            f_hit = False
         if f_hit:
             self.att_anim_counter += 1
         return f_hit, is_running
+
+    def handle_parallax(self):
+        if self.bg_img_front_offset <= -SCREEN_WIDTH:
+            self.bg_img_front_offset = SCREEN_WIDTH
+        elif self.bg_img_front_offset >= SCREEN_WIDTH:
+            self.bg_img_front_offset = -SCREEN_WIDTH
+
+        if self.bg_img_mid_offset <= -SCREEN_WIDTH:
+            self.bg_img_mid_offset = 0
+        elif self.bg_img_mid_offset >= SCREEN_WIDTH:
+            self.bg_img_mid_offset = 0
+
+        if self.bg_img_back_offset <= -SCREEN_WIDTH:
+            self.bg_img_back_offset = 0
+        elif self.bg_img_back_offset >= SCREEN_WIDTH:
+            self.bg_img_back_offset = 0
